ea270262ea80b2b52320586c3f3e13c46e3ee9eb
augusto <augusto.h.amaral@gmail.com>
2019-10-31 17:49:04 -0300

    asdasd

--- src/CalassAndComponents/Usuario/Login.jsx
+++ src/CalassAndComponents/Usuario/Login.jsx
@@ -33,8 +33,10 @@ class Navbar extends Component {
     }
 
     editarPerfil() {
+        const userId = this.props.auth.user.usu_in_codigo
+        console.log(userId)
+        this.props.editUserFarmacia(userId)
         this.changeOpen();
-        location.replace('#/editFarmacia')
     }
 
     render() {
